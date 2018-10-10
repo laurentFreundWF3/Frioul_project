5c4826759c9fd11995d8f3571c853c71c6b754c3
rouaix <daniel@rouaix.com>
2018-10-11 00:11:18 +0200

    Css

--- html/inc/wallofpictures.php
+++ html/inc/wallofpictures.php
@@ -1,9 +1,9 @@
 <div class="WallOfPictures">
-  <div class="liste">
-    <div class="align-item margin-bottom">
-      <h4 class="text-center col-xs-12 ml-1 mr-1">Testez notre direct</h4>
-      <h2 class="text-center col-xs-12 ml-1 mr-1">Publiez une photo avec</h2>
-      <h4 class="text-center col-xs-12 ml-1 mr-1 hashtag"> #code4marseille</h4>
+  <div class="liste margin-bottom">
+    <div class="align-item margin-bottom margin-top">
+      <h4 class="text-center col-xs-12">Testez notre direct</h4>
+      <h2 class="text-center col-xs-12">Publiez une photo avec</h2>
+      <h4 class="text-center col-xs-12 hashtag"> #code4marseille</h4>
     </div>
       <!-- ICI ON VA CREER UNE BALISE div PAR INFO RECUPERE -->
   </div>
